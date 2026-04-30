f85c5f210e6d9160e031bc950a11171ce0d359fb
valentine39 <valentine.iat@hotmail.fr>
2026-04-30 11:19:39 +0200

    Update banque_mondiale.py

--- scrapers/banque_mondiale.py
+++ scrapers/banque_mondiale.py
@@ -1,8 +1,1 @@
-def fetch_all(self, country_code: str) -> dict:
-    results = {}
 
-    for key, meta in CONFIG.INDICATORS.items():
-        df = self.fetch(meta["code"], country_code)
-        results[key] = df
-
-    return results
